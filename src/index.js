86d0a9fe5848a7534d2e6e624ae4a084de7e9c93
MarycieloParionaBernaola <mparionabernaola@gmail.com>
2020-07-26 16:19:36 -0500

    Final versión 2.1

--- src/index.js
+++ src/index.js
@@ -35,57 +35,19 @@ decodeButton.addEventListener('click', () => {
 
 const cleanButton = document.getElementById("cleanbutton");
 cleanButton.addEventListener('click', () => {
-    document.getElementById("entertext").value = ""; 
-    document.getElementById("key").value = ""; 
-    document.getElementById("outputtext").value = ""; 
+    document.getElementById("entertext").value = ''; 
+    document.getElementById("key").value = ''; 
+    document.getElementById("outputtext").value = ''; 
+})
+
+
+const sendWhatsapp= document.querySelector("#sendwhatsapp"),
+    outPutTextFinal = document.querySelector("#outputtext");
+sendWhatsapp.addEventListener("click", () => {
+    let mensaje = outPutTextFinal.value;
+    if(!mensaje) return alert("escribe algo");
+    window.open("https://api.whatsapp.com/send?text=" + encodeURIComponent(mensaje));
 })
 
 
 
-
-
-
-
-
-/*
-
-const encodebutton = document.getElementById("encodebutton");
-    encodebutton.addEventListener('click', () => {
-        let enterText = document.getElementById("entertexthtml").value;
-		let offset = document.getElementById("offset").value;
-		if(parseInt(offset)<1){ 
-			alert("¡Ingrese una clave con valor positivo :)!");
-		} else {
-        document.getElementById("outputtexthtml").innerHTML= cipher.encode(offset,enterText);  
-        }
-    })
-
-
-
-const decodeButton = document.getElementById("decodebutton");
-    decodeButton.addEventListener('click', () => {
-        let enterText = document.getElementById("entertexthtml").value;
-        let offset = document.getElementById("offset").value;
-        if(parseInt(offset)<1){ 
-            alert("¡Ingrese una clave con valor positivo :)!");
-        } else { 
-             document.getElementById("outputtexthtml").innerHTML= cipher.decode(offset,enterText);
-            }  
-    })
-
-
-const restoreButton = document.getElementById("restorebutton");
-restoreButton.addEventListener('click', () => {
-	document.getElementById("outputtexthtml").innerHTML= "";   
-	document.getElementById("entertexthtml").value = ""; 
-	document.getElementById("offset").value = ""; 
-	})
-
-
-const startButton = document.getElementById("enjoybutton");
-startButton.addEventListener('click', () => {
-    document.getElementById("firstscreen").style.display = "none";
-	document.getElementById("secondscreen").style.display = "block";
-})
-
-*/
